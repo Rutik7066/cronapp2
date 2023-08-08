f3d433b19a316d291d10a89a9a137a827792bc84
Rutik7066 <rutikthakre@gmail.com>
2023-08-08 17:29:09 +0530

    url

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,3 +1,3 @@
-const backend_url = 'http://localhost';
+const backend_url = 'http://ec2-13-200-79-156.ap-south-1.compute.amazonaws.com:';
 export const whatsapp_backend_url = `${backend_url}:8080`;
 export const pocketbase_url = `${backend_url}:5000`;
